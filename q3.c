fea08c3edf7e7840254ba6dc050e5eb59a99561d
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-12 00:19:22 +0530

    q3.c

---
+++ q3.c
@@ -0,0 +1,15 @@
+
+//Q2. Write a C program to subtract two numbers.
+
+#include<stdio.h>
+int main()
+{
+int a,b,diff;
+printf("enter first number=");
+scanf("%d",&a);
+printf("enter second number=");
+scanf("%d",&b);
+diff=a-b;
+printf("enter sum of two numbers=%d",diff);
+return 0;
+}
