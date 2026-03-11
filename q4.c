aa62422901899f60fbec52f3a49220fbac4c998a
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-12 00:20:21 +0530

    q4.c

---
+++ q4.c
@@ -0,0 +1,15 @@
+
+//Q3.Write a C program to multiply two numbers.
+
+#include<stdio.h>
+int main()
+{
+int a,b,mul;
+printf("enter first number=");
+scanf("%d",&a);
+printf("enter second number=");
+scanf("%d",&b);
+mul=a*b;
+printf("enter sum of two numbers=%d",mul);
+return 0;
+}
