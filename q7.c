758145cff128abfcd1767a24c55a8e57117ed103
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-14 01:08:07 +0530

    q7.c

---
+++ q7.c
@@ -0,0 +1,12 @@
+//Q7.Write a C program to find the cube of a number.
+
+#include<stdio.h>
+int main()
+{
+    int a,cube;
+    printf("enter number");
+    scanf("%d",&a);
+    cube=a*a*a;
+    printf("cube of a number %d",cube);
+    return 0;
+}
