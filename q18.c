c69ce7ea7eb9f11f489954534aeaa671d9fb4681
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-19 11:12:49 +0530

    q18.c

---
+++ q18.c
@@ -0,0 +1,19 @@
+
+//Q.Write a C program to check whether a person is eligible to vote.
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    if(a>=18)
+    {
+        printf("eligible to vote");
+    }
+    else
+    {
+        printf("not eligible to vote");
+    }
+    return 0;
+}
