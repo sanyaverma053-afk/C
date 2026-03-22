b5d136b0eeb1e4ad836db38da4d37e410ea351e2
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-23 00:12:15 +0530

    q28.c

---
+++ q28.c
@@ -0,0 +1,27 @@
+
+//Q17.Write a program to determine the type of triangle (equilateral, isosceles, scalene)
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter three side");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a=b=c)
+    {
+        printf("equilateral triangle");
+    }
+    else if(a==b || b==c ||a==c)
+    {
+        printf("isosceles triangle");
+    }
+    else if(a!=b && b!=c && c!=a)
+    {
+        printf("scalene triangle");
+    }
+    else
+      {
+        printf("not valid");
+    }
+    return 0;
+}
