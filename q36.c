2660e96975c9d8c719dd5e4caa7a52180e8a26ad
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-24 23:18:10 +0530

    q36.c

---
+++ q36.c
@@ -0,0 +1,16 @@
+
+//Q.Print even numbers from 1 to 20.
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=20;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
