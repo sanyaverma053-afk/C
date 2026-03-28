924af0dddf146c10245144744645a7069481a4b4
SANYA VERMA <sanyaverma053@gmail.com>
2026-03-28 23:27:33 +0530

    q55.c

---
+++ q55.c
@@ -0,0 +1,27 @@
+
+//Q.Print this pattern.
+/*
+*****
+****
+***
+**
+*
+*/
+
+
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("enter number ");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+    printf("\n");
+    }
+    return 0;
+}
