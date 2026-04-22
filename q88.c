3a0456267bda36e37cf8bfdb07fe79801f831dca
SANYA VERMA <sanyaverma053@gmail.com>
2026-04-22 23:57:57 +0530

    Add program to find the length of a string

---
+++ q88.c
@@ -0,0 +1,15 @@
+
+//WAP to find the length of string with using string function.
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[50];
+    int l;
+    printf("enter string:");
+    scanf("%s",s);
+    l=strlen(s);
+    printf("length of sting :%d",l);
+    return 0;
+}
