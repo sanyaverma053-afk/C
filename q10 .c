5a6f2db3bc3af359d77928a730c5d486161d5e05
SANYA VERMA <sanyaverma053@gmail.com>
2026-05-06 08:39:58 +0530

    Add C program to swap two numbers using a temp variable

---
+++ q10 .c
@@ -0,0 +1,18 @@
+//Q10.Write a C program to swap two numbers using a temporary variable.
+
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter a=");
+    scanf("%d",&a);
+    printf("enter b=");
+    scanf("%d",&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("enter swap number%d\n",a);
+    printf("enter another swaped number %d",b);
+    return 0;
+}
