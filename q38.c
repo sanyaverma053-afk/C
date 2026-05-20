1c72a9fd18ac155f2f5d02a7bd9fc8da56639868
SANYA VERMA <sanyaverma053@gmail.com>
2026-05-20 21:37:19 +0530

    Implement sum calculation from 1 to N
    Fix variable name and initialize sum variable.

---
+++ q38.c
@@ -0,0 +1,16 @@
+
+//Q.Find the sum of numbers from 1 to N.
+
+#include<stdio.h>
+int main()
+{
+    int n,i,s;
+    printf("enter number");
+    scanf("%d",&numbers);
+    for(i=1;i<=n;i++)
+    {
+        s=s+i;
+    }
+    printf("sum of numbers%d\n",s);
+    return 0;
+}
