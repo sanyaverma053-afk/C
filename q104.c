bc7358e6bcf0120a3c0b8d8963e546315a0244c5
SANYA VERMA <sanyaverma053@gmail.com>
2026-05-20 21:51:20 +0530

    Create a C program for book structure and pricing
    Implement a C program to manage book details and display price if pages are 200.

---
+++ q104.c
@@ -0,0 +1,34 @@
+/*
+Q.Write a C program using structure book with members title, pages, and price. 
+Take input from the user. If pages are 200, print the book price.
+*/
+
+#include<stdio.h>
+
+struct book
+{
+    char title[20];
+    int pages;
+    int price;
+};
+
+int main()
+{
+    struct book b;
+
+    printf("enter title ");
+    scanf("%s",b.title);
+
+    printf("enter pages ");
+    scanf("%d",&b.pages);
+
+    printf("enter price ");
+    scanf("%d",&b.price);
+
+    if(b.pages==200)
+    {
+        printf("book price is %d",b.price);
+    }
+
+    return 0;
+}
