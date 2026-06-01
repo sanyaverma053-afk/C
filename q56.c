e18f405941d524027cb72ab8ea86b0c027499eec
SANYA VERMA <sanyaverma053@gmail.com>
2026-06-01 15:31:02 +0530

    Add number pyramid printing program in q56.c

---
+++ q56.c
@@ -0,0 +1,24 @@
+
+//Q3.Print number pyramid.
+/*
+1
+12
+123
+1234
+12345
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for(i = 1; i <= 5; i++)   
+    {
+        for(j = 1; j <= i; j++)   
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
