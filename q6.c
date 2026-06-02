3284dbc40562bb29b683408727c4c330b839887d
SANYA VERMA <sanyaverma053@gmail.com>
2026-06-02 20:07:04 +0530

    Add C program to calculate square of a number

---
+++ q6.c
@@ -0,0 +1,12 @@
+//Q6.Write a C program to find the square of a number.
+
+#include<stdio.h>
+int main()
+{
+    int a,square;
+    printf("enter number");
+    scanf("%d",&a);
+    square=a*a;
+    printf("square of a number %d",square);
+    return 0;
+}
